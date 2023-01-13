52dcb8c01d36ab10d3e1e96446a4196398b1e998
Gaia Zanchi <gaia.zanchi2001@gmail.com>
2023-01-13 12:00:39 +0100

    while1 done

--- while1/src/main/java/it/develhope/Main.java
+++ while1/src/main/java/it/develhope/Main.java
@@ -4,13 +4,15 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        long time = TimeUnit.SECONDS.
-        time = new TimeUnit();
+    public static void main(String[] args){
 
         while(true){
-
+            try{
+                TimeUnit.SECONDS.sleep(1);
+            }catch(Exception e){
+                System.out.println("Error");
+            }
+            System.out.println("I am in loop!");
         }
 
     }
